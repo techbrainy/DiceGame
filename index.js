f330e38673f1fe363f0315e2beb7cdde1e36b641
techbrainy <techbrainy97@gmail.com>
2021-03-29 15:14:53 +0530

    code added

---
+++ index.js
@@ -0,0 +1,32 @@
+
+
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomImage = "images/dice"+ randomNumber1 + ".png";
+
+var img1 = document.querySelectorAll("img")[0];
+
+img1.setAttribute("src", randomImage);
+
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImage = "images/dice"+ randomNumber2 + ".png";
+
+var img2 = document.querySelectorAll("img")[1];
+
+img2.setAttribute("src", randomImage);
+
+
+if(randomNumber1 == randomNumber2)
+{
+  document.querySelector("h1").innerHTML = "Draw";
+}
+else if(randomNumber1 > randomNumber2)
+{
+  document.querySelector("h1").innerHTML = "Player 1 wins";
+}
+else{
+  document.querySelector("h1").innerHTML = "Player 2 wins";
+}
